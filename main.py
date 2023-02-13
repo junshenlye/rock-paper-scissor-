c6b74d10a67a2b6f51bb3a3a1f4cc1324d813205
junshenlye <69711881+junshenlye@users.noreply.github.com>
2023-02-13 22:22:41 +0000

    main

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import random
 
 options = ["rock", "paper", "scissor"]
+def check_winners(player, computer):
+    if player == computer:
+      print("Its a Tie!")
+    elif player == "rock" and computer == "scissor":
+      print("Congrates, You Won!. Rock Beats Scissor.")
+    elif player == "scissor" and computer == "paper":
+      print("Congrates, You Won!. Scissor Beats Paper")
+    elif player == "paper" and computer == "rock":
+      print("Congrates, You Won!. Paper Beats Rock.")
+    else:
+      computer = computer.capitalize()
+      player = player.capitalize()
+      print("You lost! {} beats {}.".format(computer, player))
 
-
-def game():
+while True:
   player_choice = str(input("Choose Rock, Paper or Scissor!")).lower()
   if player_choice in options:
     computer_choice = random.choice(options)
-    if player_choice == computer_choice:
-      print("Its a Tie!")
-    elif player_choice == "rock" and computer_choice == "scissor":
-      print("Congrates, You Won!. Rock Beats Scissor.")
-    elif player_choice == "scissor" and computer_choice == "paper":
-      print("Congrates, You Won!. Scissor Beats Paper")
-    elif player_choice == "paper" and computer_choice == "rock":
-      print("Congrates, You Won!. Paper Beats Rock.")
-    else:
-      computer_choice = computer_choice.capitalize()
-      player_choice = player_choice.capitalize()
-      print("You lost! {} beats {}.".format(computer_choice, player_choice))
-
+    check_winners(player_choice, computer_choice)
   else:
     print("Invalid Option, Please Try Again!")
-
-
-while True:
-  game()
+  
